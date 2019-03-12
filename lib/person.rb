37063a78c41fc6a8e11fc1a721305f25e8216ef3
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-12 02:01:40 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,12 +3,12 @@ class Person
     @this_persons_name = person_name
   end
   def name
-    @this_persons_name
+    puts @this_persons_name
   end
   def job=(person_job)
     @this_persons_job = person_job
   end
   def job
-    @this_persons_job
+    puts @this_persons_job
   end
 end
