854eb1896510186733a184a64bd8bf7478aeeabd
Christopher Jacobs <chrisj.3222@gmail.com>
2019-03-12 02:02:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
   def name=(dog_name)
-    @this_dogs_name = dog_name
+    @name = dog_name
   end
   def name
-    puts @this_dogs_name
+    puts @name
   end
   def breed=(dog_breed)
-    @this_dogs_breed = dog_breed
+    @breed = dog_breed
   end
   def breed
-    puts @this_dogs_breed
+    puts @breed
   end
 end
